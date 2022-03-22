a4dd268465b30f5e57101293baebc65c467f3b8f
jvgouvea <contato.jvgouvea@gmail.com>
2022-03-22 04:26:09 -0300

    organizacao js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,50 @@
 // Trocar texto ao clique na lista de imagens
 
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent div");
-tabContent[0].classList.add("ativo");
+function initTabMenu() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent div");
 
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index){
-    tabContent.forEach((conteudo) => {
-      conteudo.classList.remove("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((conteudo) => {
+        conteudo.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
   }
-  
-  tabMenu.forEach((item,index) => {
-    item.addEventListener("click",() => {
-      activeTab(index);
-    })
-  });
 }
+
+initTabMenu();
 
 // Mostrar e ocultar o texto ao clique
 
-const faqdt = document.querySelectorAll(".js-hidden dt");
-const faqdd = document.querySelectorAll(".js-hidden dd");
-faqdd[0].classList.add("ativo");
-faqdt[0].classList.add("setadown");
+function initMostrarTexto() {
+  const faqdt = document.querySelectorAll(".js-hidden dt");
+  const faqdd = document.querySelectorAll(".js-hidden dd");
 
-function mostrarTexto(index) {
-  faqdd[index].classList.toggle("ativo");
-  faqdt[index].classList.toggle("setadown");
+  if (faqdd.length && faqdt.length) {
+    faqdd[0].classList.add("ativo");
+    faqdt[0].classList.add("setadown");
+
+    function mostrarTexto(index) {
+      faqdd[index].classList.toggle("ativo");
+      faqdt[index].classList.toggle("setadown");
+    }
+
+    faqdt.forEach((dt, index) => {
+      dt.addEventListener("click", () => {
+        mostrarTexto(index);
+      });
+    });
+  }
 }
 
-faqdt.forEach((dt,index) => {
-  dt.addEventListener("click", () => {
-    mostrarTexto(index);
-  })
-});
-
+initMostrarTexto();

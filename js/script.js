b3622032647fdda6c362cf0c6949053fb1186b91
jvgouvea <contato.jvgouvea@gmail.com>
2022-03-28 16:20:15 -0300

    fit Scroll reveal

--- js/script.js
+++ js/script.js
@@ -48,3 +48,24 @@ function initMostrarTexto() {
 }
 
 initMostrarTexto();
+
+function initScrollReveal() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const calculoWindow = window.innerHeight * 0.6;
+
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((item) => {
+        const distanciaTopo = item.getBoundingClientRect().top - calculoWindow;
+
+        if (distanciaTopo < 10) {
+          item.classList.add("ativo");
+        }
+      });
+    }
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initScrollReveal();

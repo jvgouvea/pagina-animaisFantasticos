ad9679170d70ca54a4a2ebca5d1865e40630c7a5
jvgouvea <contato.jvgouvea@gmail.com>
2022-04-07 22:06:34 -0300

    fit tooltip

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-import initTabMenu from './modules/tabmenu.js';
-import initMostrarTexto from './modules/hiddentexto.js';
-import initScrollReveal from './modules/scrollreveal.js';
-import initModal from './modules/modal.js';
+import initTabMenu from "./modules/tabmenu.js";
+import initMostrarTexto from "./modules/hiddentexto.js";
+import initScrollReveal from "./modules/scrollreveal.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabMenu();
 initMostrarTexto();
 initScrollReveal();
 initModal();
+initTooltip();
